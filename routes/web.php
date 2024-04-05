14948e98e9d4f3d9c6759d0847f273170bfd8c6e
2272046_ImmanuelC <immanuelnuel005+github@gmail.com>
2024-04-05 09:54:47 +0700

    Push 2

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthControllers;
+use App\Http\Controllers\HomeControllers;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +19,23 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/about', function(){
+    return view('about');
+})->name('route-about');
+
+Route::get('/form', [\App\Http\Controllers\PageController::class, 'create'])->name('route-form');
+
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthControllers::class, 'register'])->name('register');
+    Route::post('/register', [AuthControllers::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthControllers::class, 'login'])->name('login');
+    Route::post('/login', [AuthControllers::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeControllers::class, 'index']);
+    Route::delete('/logout', [AuthControllers::class, 'logout'])->name('logout');
+});
+
